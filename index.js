cb3d6d25efb38c917d47dbc3c948ee15b477cf4f
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-29 14:48:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(arr, str) {
+    let newArr = arr.filter((item) => item.localeCompare(str, undefined, { sensitivity: 'accent' }) === 0);
+    return newArr;
+}
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(arr, str) {
+    let newArr = arr.filter((item) => {
+        let num = str.length;
+        if (item.slice(0, num) === str) {
+            return item
+        }
+    });
+    return newArr;
+}
+
+// console.log(fuzzyMatch(drivers, "Sa"))
+
+function matchName(arr, str) {
+    let newArr = arr.filter((item) => item.name === str);
+    return newArr;
+}
+// console.log(drivers)
+// console.log(matchName(drivers, "Bobby"))
+
+
+
